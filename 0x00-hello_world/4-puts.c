4006fc5ead858ce9993969f58019f63590e39c1b
Liloxi <ozigbejjohn@gmail.com>
2022-03-14 14:06:19 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * return: Always 0 (sucess)
+ */
+int main(void)
+{ 
+	puts("\"programming is like building a multiliginual puzzle");
+	return (0);
+}
